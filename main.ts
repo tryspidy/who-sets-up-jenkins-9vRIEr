0c93c308d9135a5cb745a9d600e2acdce10724a6
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-09 12:03:52 +0530

    Initial commit

---
+++ main.ts
@@ -0,0 +1,5 @@
+In my company we have DevOps/Operations Support team who is responsible 
+for maintaining jenkins.
+ Those installed and configured jenkins. for deployments etc. They work with 
+  developers to create jobs for building and deploying the applications.
+ For creating smoke tests, they work with automation engineers
